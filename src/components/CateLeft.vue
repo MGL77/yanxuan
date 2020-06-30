b45935e57331b545725188323d1ad8038b779330
MGL77 <13694732649@qq.com>
2020-06-30 22:04:33 +0800

    划分出来分类的一小块组件

---
+++ src/components/CateLeft.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <b>内蒙古</b>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="">
+    
+</style>
--- src/pages/Cate.vue
+++ src/pages/Cate.vue
@@ -1,13 +1,17 @@
 <template>
-    <div>
-        <b>分类</b>
-    </div>
+  <div>
+    <!-- 左侧列表版块开始 -->
+    <CateLeft />
+    <!-- 左侧列表版块结束 -->
+  </div>
 </template>
 <script>
+import CateLeft from "@/components/CateLeft.vue";
 export default {
-    
-}
+  components: {
+    CateLeft
+  }
+};
 </script>
 <style lang="less">
-    
 </style>
